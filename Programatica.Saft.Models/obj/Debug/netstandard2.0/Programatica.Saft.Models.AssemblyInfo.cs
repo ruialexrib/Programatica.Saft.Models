83f0df4ba57157aa83d12629db77fb1257a04efb
Rui Ribeiro <ruialexrib@gmail.com>
2020-03-06 14:54:59 +0000

    changed package description

--- Programatica.Saft.Models/obj/Debug/netstandard2.0/Programatica.Saft.Models.AssemblyInfo.cs
+++ Programatica.Saft.Models/obj/Debug/netstandard2.0/Programatica.Saft.Models.AssemblyInfo.cs
@@ -14,7 +14,8 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programatica Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©  Programatica Software 2020")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Models for SAFT-PT file")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Object Model for SAFT-PT file (Standard Audit File for Tax Purposes - Portugal Ve" +
+    "rsion)")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programatica.Saft.Models")]
